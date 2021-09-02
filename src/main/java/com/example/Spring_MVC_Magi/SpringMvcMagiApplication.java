7d3c3293bf659f9f53afdf9883746a563820e9ee
Ulysses <romeroulysses3@gmail.com>
2021-09-01 21:54:20 -0500

    first commit

---
+++ src/main/java/com/example/Spring_MVC_Magi/SpringMvcMagiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spring_MVC_Magi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcMagiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcMagiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Spring_MVC_Magi/SpringMvcMagiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring_MVC_Magi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcMagiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
