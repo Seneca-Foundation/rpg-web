f0f9cb900afe4176f53cac315e45660ce026b5dc
Ulysses <romeroulysses3@gmail.com>
2021-09-15 12:37:42 -0500

    Response Entity

--- src/main/java/com/senecafoundation/Scene/Response.java
+++ src/main/java/com/senecafoundation/Scene/Response.java
@@ -1,20 +1,44 @@
 package com.senecafoundation.Scene;
 
+import javax.persistence.Entity;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Entity
 public class Response 
 {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
     //Variables
+    private String id;
     private String responseText;
     private String resultingSceneIDFromChoce;
     
     public Response(String responseText, String resultingSceneIDFromChoce) {
+        this.id = UUID.randomUUID().toString();
         this.responseText = responseText;
         this.resultingSceneIDFromChoce = resultingSceneIDFromChoce;
     }
-    public String getResultingSceneIDFromChoice() {
+
+    public Response() {}
+
+    public String getId() 
+    {
+        return id;
+    }
+
+    public String getResultingSceneIDFromChoice() 
+    {
         return resultingSceneIDFromChoce;
     }
 
-    public String getResponseText() {
+    public String getResponseText() 
+    {
         return responseText;
     }
 }
