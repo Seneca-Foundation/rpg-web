e9660b2ada41a11d4428077b6e35afe16747aec3
Ulysses <romeroulysses3@gmail.com>
2021-09-14 22:43:03 -0500

    @Id, @Column and @GeneratedValue disallowed error

--- src/main/java/com/senecafoundation/Scene/Choice.java
+++ src/main/java/com/senecafoundation/Scene/Choice.java
@@ -1,30 +1,52 @@
 package com.senecafoundation.Scene;
 
 import javax.persistence.Entity;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.UUID;
 
 @Entity
 public class Choice
 {
     //Variables
+    private String id;
     private int choiceOptionNumber;
     private String choiceText;
     private Response response;
 
-    public Choice(int choiceOptionNumber, String choiceText, Response response) {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    public Choice(int choiceOptionNumber, String choiceText, Response response) 
+    {
+        this.id = UUID.randomUUID().toString();
         this.choiceOptionNumber = choiceOptionNumber;
         this.response = response;
         this.choiceText = choiceText;
     }
 
-    public int getChoiceOptionNumber() {
+    public Choice() {}
+
+    public String getId() 
+    {
+        return id;
+    }
+
+    public int getChoiceOptionNumber() 
+    {
         return choiceOptionNumber;
     }
 
-    public Response getResponse() {
+    public Response getResponse() 
+    {
         return response;
     }
 
-    public String getChoiceText() {
+    public String getChoiceText() 
+    {
         return choiceText;
     }
 }
