ee9000d692486747bd3fa0f7ec132aa6e5df5801
Brett Yates <brettyates@gmail.com>
2021-09-22 12:10:19 -0700

    base method in ShadowElfController for getCharacter, import Optional util in DataHandler for ReadCharacter

--- src/main/java/com/senecafoundation/ShadowElfController.java
+++ src/main/java/com/senecafoundation/ShadowElfController.java
@@ -1,5 +1,9 @@
 package com.senecafoundation;
 
+import java.util.Optional;
+import java.util.UUID;
+
+import com.senecafoundation.CharacterTypes.Character;
 import com.senecafoundation.CharacterTypes.ShadowElf;
 import com.senecafoundation.DataHandler.RepoDataHandler;
 
@@ -10,6 +14,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -25,6 +30,11 @@ public class ShadowElfController {
         model.addAttribute("shadowelf", shadowElf);
         return "create_shadowelf";
     }
+
+    @RequestMapping(value = "/{id}")
+	 public Optional<Character> getCharacter(@PathVariable UUID id) {
+	 	return dataHandler.ReadCharacter(id);
+	 }
 
     @RequestMapping(value = "/createform", method = RequestMethod.POST)
     public String submit(@ModelAttribute("shadowelf") ShadowElf shadowElf, BindingResult result, ModelMap model) {
