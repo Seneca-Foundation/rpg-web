39057db9fa588b1a02bd9691567e6804065acc49
Ulysses <romeroulysses3@gmail.com>
2021-10-06 22:36:26 -0500

    Got this running >:)

--- src/main/java/com/senecafoundation/NPCController.java
+++ src/main/java/com/senecafoundation/NPCController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("Npc")
 public class NPCController 
 {
-    UUID id;
     @Autowired
     NpcDataHandler dataHandler;
 
@@ -55,12 +54,13 @@ public class NPCController
         model.addAttribute("Npc", npc);
         return "create_Npc"; 
     }
-    @RequestMapping(value="/updateform", method = RequestMethod.PUT)
+    @RequestMapping(value="/updateform", method = RequestMethod.POST)
     public String change(NPC npc, BindingResult result, ModelMap model) {
         if (result.hasErrors()) {
-            return "Error";
+            return "error";
         }
         dataHandler.Update(npc);
+        model.addAttribute("Npc",npc);
         return "Npc";   
     }
 
