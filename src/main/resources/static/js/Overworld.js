8e98a23138e0327ecdc1e338691483df3a0b83f9
Ulysses <romeroulysses3@gmail.com>
2021-10-22 17:45:34 -0500

    Continued

--- src/main/resources/static/js/Overworld.js
+++ src/main/resources/static/js/Overworld.js
@@ -5,37 +5,41 @@ class Overworld //top level parent component
         this.element = config.element; //pass in element for overworld to operate on
         this.canvas = this.element.querySelector(".game-canvas"); // grabs canvas tag from style.css
         this.ctx = this.canvas.getContext("2d"); // drawing from the context, gives access to alot of drawing methods that exists in canvas element
+        this.map = null;
+    }
+
+    startGameLoop() {
+        const step = () => { //actual funtion that calls every frame
+
+            //Clears off the canvas
+            this.ctx.clearRect(0,0, this.canvas.width, this.canvas.height);
+
+            //Draw Lower Layer
+            this.map.drawLowerImage(this.ctx);
+
+            //Draw Game Objects  *Note* game obejct stored as an object of game objects;  Object.values ---> take values of key value store and iterate through them
+            Object.values(this.map.gameObjects).forEach(object => {
+                object.x += 0.02;
+                object.sprite.draw(this.ctx);
+            })
+
+            //Draw Upper Layer
+            this.map.drawUpperImage(this.ctx);
+
+
+            requestAnimationFrame(() => { // calls funtions whever new frame begins
+            step();
+            })
+        }
+        step();
     }
 
     init() 
     {
-        const image = new Image(); //create new image, assign a source to that image, copy over pixels to the canvas, ctx (context) allows for drawing on canvas
-        image.onload = () => {
-            this.ctx.drawImage(image, 0, 0)//wants a few arguments, simple: pass in the image we want to draw pixels from and then an x,ycoordinate where we wil draw image from
-        };
-        image.src = "images/DemoLower.png";
+        this.map = new OverworldMap(window.OverworldMaps.Kitchen);
+        this.startGameLoop();
 
-
-        //layering effects, character goes after background
-        //counting by 16x16 cells top left corner of map; characters 32x32
-
-        //Place some Game Objects!
-        const hero = new GameObject({
-            x: 5,
-            y: 6,
-            src: "images/test.png"
-        })
-        const npc1 = new GameObject({
-            x: 7,
-            y: 9,
-            src: "images/shadowelf.png"
-        })
-
-
-        setTimeout(() => {
-            hero.sprite.draw(this.ctx);
-            npc1.sprite.draw(this.ctx);
-        }, 200)
+        
         
 
 
---
+++ src/main/resources/static/js/OverworldMap.js
@@ -0,0 +1,56 @@
+class OverworldMap {
+    constructor(config) {
+        this.gameObjects = config.gameObjects;
+
+        this.lowerImage = new Image();
+        this.lowerImage.src = config.lowerSrc; // lower layer-contains tiles floor where character will stand on
+
+        this.upperImage = new Image();
+        this.upperImage.src = config.upperSrc; // upper layers- rooftops and etc
+    }
+
+    drawLowerImage(ctx) {
+        ctx.drawImage(this.lowerImage, 0, 0) // draws lower images
+    }
+
+    drawUpperImage(ctx) {
+        ctx.drawImage(this.upperImage, 0, 0) // draws upper images
+    }
+}
+    window.OverworldMaps = {
+        DemoRoom: {
+            lowerSrc: "images/DemoLower.png",
+            upperSrc: "images/DemoUpper.png",
+            gameObjects: {
+                hero: new GameObject({
+                    x: 5,
+                    y: 6,
+                }),
+                npc1: new GameObject({
+                    x: 7,
+                    y: 6,
+                    src: "images/shadowelf.png"
+                })
+            }
+        },
+        Kitchen: {
+            lowerSrc: "images/KitchenLower.png",
+            upperSrc: "images/KitchenUpper.png",
+            gameObjects: {
+                hero: new GameObject({
+                    x: 3,
+                    y: 5,
+                }),
+                npcA: new GameObject({
+                    x: 9,
+                    y: 6,
+                    src: "images/test.png"
+                }),
+                npcB: new GameObject({
+                    x: 10,
+                    y: 8,
+                    src: "images/shadowelf.png"
+                })
+            }
+        },
+    }
